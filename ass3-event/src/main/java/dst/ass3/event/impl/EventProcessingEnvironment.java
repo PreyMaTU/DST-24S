22953db51a5eb1632ef130958ffcbae7a795f49a
PreyMa <mlpreymann@gmail.com>
2024-06-02 23:11:39 +0200

    Task 3.3.4: Implement Stream Window Operators

--- ass3-event/src/main/java/dst/ass3/event/impl/EventProcessingEnvironment.java
+++ ass3-event/src/main/java/dst/ass3/event/impl/EventProcessingEnvironment.java
@@ -1,26 +1,26 @@
 package dst.ass3.event.impl;
 
 import dst.ass3.event.IEventProcessingEnvironment;
-import dst.ass3.event.model.domain.TripState;
+import dst.ass3.event.model.domain.Region;
 import dst.ass3.event.model.events.*;
 import org.apache.flink.api.common.eventtime.Watermark;
 import org.apache.flink.api.common.eventtime.WatermarkGenerator;
 import org.apache.flink.api.common.eventtime.WatermarkOutput;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.cep.CEP;
-import org.apache.flink.cep.functions.PatternProcessFunction;
-import org.apache.flink.cep.functions.TimedOutPartialMatchHandler;
-import org.apache.flink.cep.pattern.Pattern;
-import org.apache.flink.cep.pattern.conditions.SimpleCondition;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.GlobalWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.triggers.CountTrigger;
+import org.apache.flink.streaming.api.windowing.windows.GlobalWindow;
 import org.apache.flink.util.Collector;
 import org.apache.flink.util.OutputTag;
 
-import java.time.Duration;
-import java.util.List;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.stream.StreamSupport;
 
 
 public class EventProcessingEnvironment implements IEventProcessingEnvironment {
@@ -34,7 +34,7 @@ public class EventProcessingEnvironment implements IEventProcessingEnvironment {
 
     @Override
     public void initialize(StreamExecutionEnvironment env) {
-        // Setup watermarking based on the event timestamp
+        // Set up watermarking based on the event timestamp
         final var watermarkStrategy= WatermarkStrategy
                 .forGenerator(context -> new PunctuatedWatermarkGenerator())
                 .withTimestampAssigner((event, timestamp) -> event.getTimestamp());
@@ -65,6 +65,48 @@ public class EventProcessingEnvironment implements IEventProcessingEnvironment {
         final var tripFailedWarningStream= badRequestsPatternStream.process( new BadRequestsPatternFunction() );
 
         tripFailedWarningStream.addSink(tripFailedWarningSink);
+
+        // Combine the two error streams with an up-cast to Warning objects
+        final var unionWarningStream= matchingDurationStream
+                .getSideOutput(timeoutOutputTag)
+                .map((MapFunction<MatchingTimeoutWarning, Warning>) event -> event)
+                .union( tripFailedWarningStream.map((MapFunction<TripFailedWarning, Warning>) event -> event) );
+
+        // Set up a window function that gets triggert whenever three warnings are received and generate an alert
+        final var alertStream= unionWarningStream
+                .keyBy( warning -> warning.getRegion().name() )
+                .window(GlobalWindows.create())
+                .trigger(CountTrigger.of(3))
+                .apply(new WindowFunction<Warning, Alert, String, GlobalWindow>() {
+                    @Override
+                    public void apply(String regionName, GlobalWindow globalWindow, Iterable<Warning> warnings, Collector<Alert> collector) throws Exception {
+                        final var warningList= new ArrayList<Warning>(3);
+                        warnings.forEach( warningList::add );
+                        collector.collect(new Alert(Region.valueOf(regionName), warningList));
+                    }
+                });
+
+        alertStream.addSink(alertSink);
+
+        // Set up a window function that gets triggert every five matching durations and calculates their average
+        final var averageMatchingDurationStream= matchingDurationStream
+                .keyBy( event -> event.getRegion().name() )
+                .window(GlobalWindows.create())
+                .trigger(CountTrigger.of(5))
+                .apply(new WindowFunction<MatchingDuration, AverageMatchingDuration, String, GlobalWindow>() {
+                    @Override
+                    public void apply(String regionName, GlobalWindow globalWindow, Iterable<MatchingDuration> durations, Collector<AverageMatchingDuration> collector) throws Exception {
+                        final var averageDuration= StreamSupport
+                                .stream(durations.spliterator(), false)
+                                .mapToDouble(MatchingDuration::getDuration)
+                                .average()
+                                .orElse(0);
+
+                        collector.collect( new AverageMatchingDuration(Region.valueOf(regionName), averageDuration));
+                    }
+                });
+
+        averageMatchingDurationStream.addSink( averageMatchingDurationSink );
     }
 
     @Override
